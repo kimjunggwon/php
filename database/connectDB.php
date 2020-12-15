dce3494373b3853d9c72178fac686283ef85b3f0
kimjunggwon <overroading@gmail.com>
2020-12-15 11:51:18 +0900

    php_database

--- database/connectDB.php
+++ database/connectDB.php
@@ -1,10 +1,11 @@
 <meta charset="utf-8">
 <?php
-    $host = 'localhost';
-    $user = 'root';
-    $pw = 'apmsetup';
+    $host   = 'localhost';
+    $user   = 'root';
+    $pw     = 'apmsetup';
+    $dbName = 'study';
     //DB 접속 정보 변수 설정
-    $dbConnect = new mysqli($host, $user, $pw);
+    $dbConnect = new mysqli($host, $user, $pw, $dbName);
     //mysqli 생성자를 이용하여 DB정보 저장
     $dbConnect -> set_charset('utf-8');
     //DB 정보가 들어갈 때 utf-8로 셋팅
---
+++ database/createDB.php
@@ -0,0 +1,27 @@
+<meta charset="utf-8">
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $pw   = 'apmsetup';
+    $dbConnect = new mysqli($host, $user, $pw);
+    $dbConnect -> set_charset('utf-8');
+
+    if(mysqli_connect_errno()){
+        echo "데이터 베이스 접속 실패";
+    }else{
+        $sql ="CREATE DATABASE study";
+        $res = $dbConnect -> query($sql);
+
+        if($res){
+            echo "데이터 베이스 생성 완료";
+        }else{
+            echo "데이터 베이스 생성 실패";
+        }
+    }
+
+    /*
+    query() : DB 접속이 정상적으로 연결 시, 쿼리문을 사용할 수 있는 메소드
+     - query() 구조 : query(쿼리문)
+    쿼리문 : CREATE DATABASE study -> study 데이터 베이스를 생성
+    */
+?>
