ddeccead78ab87999efa1554e00be69dbe05761e
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-03-20 00:12:15 +0900

    variable

---
+++ php_basic/variableRule.php
@@ -0,0 +1,24 @@
+<?php
+    $num2 = "변수명 num2";
+    echo $num2;
+    echo "<br>";
+
+    $num3num = "변수명 num3num";
+    echo $num3num;
+    echo "<br>";
+
+    $_num = "변수명 _num";
+    echo $_num;
+    echo "<br>";
+
+    $Num = "변수명 Num";
+    echo $Num;
+    echo "<br>";
+
+    $num = "변수명 num";
+    echo $num;
+    echo "<br>";
+
+    $str = "Hello World \"안녕하세요\"";
+    echo $str;
+?>
