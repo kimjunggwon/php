d455ce9f2a1d51170253edf9bf09d17b3b2f7558
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-03-20 00:14:59 +0900

    operator

---
+++ php_basic/operator.php
@@ -0,0 +1,16 @@
+<?php
+    //변수와 변수 연결
+    $str = "Hello ";
+    $str2 = "World";
+
+    echo $str.$str2;
+    echo "<br>";
+
+    //문자열과 문자열 연결
+    echo "Hello "."World";
+    echo "<br>";
+
+    //변수와 문자열 연결
+    $str = "Hello ";
+    echo $str."World";
+?>
