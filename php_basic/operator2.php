52603468c1dc31a911165c6f860bc2857fcab405
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-03-20 00:54:03 +0900

    operator

---
+++ php_basic/operator2.php
@@ -0,0 +1,20 @@
+<?php
+    $sum = 1 + 1;
+    echo "1 더하기 1은 ".$sum;
+    echo "<br>";
+
+    $minus = 2 - 1;
+    echo "2 빼기 1은 ".$minus;
+    echo "<br>";
+
+    $mul = 2 * 2;
+    echo "2 곱하기 2는 ".$mul;
+    echo "<br>";
+
+    $division = 9 / 3;
+    echo "9 나누기 3은 ".$division;
+    echo "<br>";
+
+    $rest = 5 % 3;
+    echo "5 나누기 3의 나머지 값은 ".$rest;
+?>
