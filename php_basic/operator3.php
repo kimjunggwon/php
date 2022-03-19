a645590f4fc2c3b2c528d759d8dd2cd85c42e682
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-03-20 01:05:28 +0900

    Assignment Operator

---
+++ php_basic/operator3.php
@@ -0,0 +1,25 @@
+<?php
+    $num = 10;
+    $num += 2;
+    echo "[+=사용] 변수 num의 값은 ".$num."<br>";
+
+    $num = 10;
+    $num -= 2;
+    echo "[-=사용] 변수 num의 값은 ".$num."<br>";
+
+    $num = 10;
+    $num *= 2;
+    echo "[*=사용] 변수 num의 값은 ".$num."<br>";
+    
+    $num = 10;
+    $num /= 2;
+    echo "[/=사용] 변수 num의 값은 ".$num."<br>";
+
+    $num = 10;
+    $num %= 3;
+    echo "[%=사용] 변수 num의 값은 ".$num."<br>";
+
+    $str = "Hello ";
+    $str .= "World";
+    echo "[.=사용] 변수 str의 값은 ".$str."<br>";
+?>
