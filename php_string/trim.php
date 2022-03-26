275e48e07f2cd2ecb71a40b7291830a29fcda60c
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-03-26 23:35:57 +0900

    trim

---
+++ php_string/trim.php
@@ -0,0 +1,12 @@
+<?php
+    $str = " test ";
+    echo "|".trim($str)."|";
+    echo "<br>";
+
+    $str = " test ";
+    echo "|".ltrim($str)."|";
+    echo "<br>";
+
+    $str = " test ";
+    echo "|".rtrim($str)."|";
+?>
