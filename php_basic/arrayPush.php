a9ee0e9e60d85ebd74ad3880417270bb672b8469
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-03-26 14:38:14 +0900

    arrayPush

---
+++ php_basic/arrayPush.php
@@ -0,0 +1,11 @@
+<?php
+    $array = array();
+
+    array_push($array, 'test1', 'test2', 'test3', 'test4', 'test5');
+
+    echo $array[0].'<br>';
+    echo $array[1].'<br>';
+    echo $array[2].'<br>';
+    echo $array[3].'<br>';
+    echo $array[4]
+?>
