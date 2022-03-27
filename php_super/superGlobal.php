2a2f9d561181ad0c2bae89c7f7d425514a0c9cc3
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-03-27 20:56:41 +0900

    superglobal

---
+++ php_super/superGlobal.php
@@ -0,0 +1,11 @@
+<?php
+    echo "DOCUMENT_ROOT : ".$_SERVER['DOCUMENT_ROOT']."<br>";
+    echo "HTTP_ACCEPT_LANGUAGE : ".$_SERVER['HTTP_ACCEPT_LANGUAGE']."<br>";
+    echo "HTTP HOST : ".$_SERVER['HTTP_HOST']."<br>";
+    echo "HTTP_USER_AGENT : ".$_SERVER['HTTP_USER_AGENT']."<br>";
+    echo "SERVER_PORT : ".$_SERVER['SERVER_PORT']."<br>";
+    echo "SCRIPT_NAME : ".$_SERVER['SCRIPT_NAME']."<br>";
+    echo "REQUEST_URI : ".$_SERVER['REQUEST_URI']."<br>";
+    echo "PHP_SELF : ".$_SERVER['PHP_SELF']."<br>";
+    echo "QUERY_STRING : ".$_SERVER['QUERY_STRING'];
+?>
