ee76a8ac691226b4a5673cbd9f5a31fc57090d81
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-03-27 19:10:33 +0900

    mktime

--- php_time/mktime2.php
+++ php_time/mktime2.php
@@ -1,9 +1,12 @@
 <?php
     $startTime = mktime(0, 0, 0, 1, 1, 2022);
+    //시작 시간
 
     $endTime = mktime(0, 59, 59, 4, 1, 2022);
+    //종료 시간
 
     $nowTime = time();
+    //현재 시간
 
     if($nowTime >= $startTime && $nowTime <= $endTime){
         echo "시작 시간과 종료 시간 사이 값";
